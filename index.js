fb403ad6d672380a4b3aa422d9230638a9219440
Jubaer Jami <jamijubaer@gmail.com>
2022-02-23 21:19:27 +0600

    protected credential and created a expired data route

--- index.js
+++ index.js
@@ -13,17 +13,32 @@ const express = require("express");
 
 const admin = require("firebase-admin");
 const app = express();
-var serviceAccount = require("./driver-tracking-c7772-firebase-adminsdk-3bsnt-1e7fb70d12.json");
 
+serviceAccount = {
+  type: "service_account",
+  project_id: "driver-tracking-c7772",
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3bsnt%40driver-tracking-c7772.iam.gserviceaccount.com",
+};
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://driver-tracking-c7772-default-rtdb.firebaseio.com",
 });
 
+
 var expiryData = [];
+var expiredData = [];
 const getExpiryDates = (data) => {
   const temp = {};
   temp["email"] = data.applicantInformation.email;
+  temp["phoneNumber"] = data.applicantInformation.mobileNumber;
   temp["physicalExamExpirationDate"] =
     data.applicantInformation.physicalExamExpirationDate;
   temp["licenceExpireDates"] = data.drivingExperience.driverLicenseList.dates;
@@ -56,11 +71,15 @@ const getServerData = () => {
 const checkAndSendMail = () => {
   const todaysDate = new Date();
   expiryData.forEach((data) => {
+    var temp = { email: data.email, phoneNumber: data.phoneNumber };
+
     var mailOptions = {
       from: "trackingdriver22@gmail.com",
       to: data.email,
     };
     if (data.physicalExamExpirationDate < todaysDate) {
+      temp["physicalExamExpired"] = true;
+
       mailOptions["subject"] = "Certificate Expiration";
       mailOptions["text"] =
         "You medical certificate has expired please renew it";
@@ -73,9 +92,9 @@ const checkAndSendMail = () => {
       });
     }
     if (data.maintenanceDue < todaysDate) {
+      temp["maintenanceDue"] = true;
       mailOptions["subject"] = "Maintenance Due";
-      mailOptions["text"] =
-        "You Maintenance Due has expired please renew it";
+      mailOptions["text"] = "You Maintenance Due has expired please renew it";
       transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
           console.log(error);
@@ -86,6 +105,7 @@ const checkAndSendMail = () => {
     }
     data.licenceExpireDates.forEach((date, index) => {
       if (date < todaysDate) {
+        temp[`${data.licenceStates[index]}LicenceExpired`] = true;
         mailOptions["subject"] = "Certificate Expiration";
         mailOptions[
           "text"
@@ -99,16 +119,23 @@ const checkAndSendMail = () => {
         });
       }
     });
+
+    if (Object.keys(temp).length > 1) {
+      expiredData.push(temp);
+    }
   });
 };
 
 getServerData();
 setInterval(getServerData, 86400000);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("Email notifier every 24 Hours");
 });
+
+app.get("/expiredData", (req, res) => [res.json(expiredData)]);
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
